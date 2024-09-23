58668b0cf7957411d1d4a45388d2624bff7ac475
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-09-23 23:28:03 +0530

    auth redirectToSignIn

--- middleware.js
+++ middleware.js
@@ -1,6 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+
+const isProtectedRoute = createRouteMatcher([
+    "/dashboard(.*",
+    "/events(.*)",
+    "/meetings(.*)",
+    "/availability(.*",
+])
+
+export default clerkMiddleware((auth, req) => {
+    if(!auth().userId && isProtectedRoute(req)){
+        return auth().redirectToSignIn();
+    }
+
+});
 
 export const config = {
   matcher: [
